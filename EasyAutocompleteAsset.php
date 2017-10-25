de1192c30ff427f81b42706336a3f4cfc43d0295
gitrequests <key_dima@mail.ru>
2017-10-25 09:45:22 +0300

    Update Assets Bundle

--- EasyAutocompleteAsset.php
+++ EasyAutocompleteAsset.php
@@ -14,19 +14,12 @@ use yii\web\AssetBundle;
  */
 class EasyAutocompleteAsset extends AssetBundle
 {
+    public $sourcePath = '@bower/EasyAutocomplete/dist';
 	public $css = [
-		[
-			'https://cdnjs.cloudflare.com/ajax/libs/easy-autocomplete/1.3.5/easy-autocomplete.min.css',
-			'integrity' => 'sha256-fARYVJfhP7LIqNnfUtpnbujW34NsfC4OJbtc37rK2rs=',
-			'crossorigin' => 'anonymous'
-		]
+		'easy-autocomplete.min.css'
 	];
     public $js = [
-	    [
-		    'https://cdnjs.cloudflare.com/ajax/libs/easy-autocomplete/1.3.5/jquery.easy-autocomplete.min.js',
-		    'integrity' => 'sha256-aS5HnZXPFUnMTBhNEiZ+fKMsekyUqwm30faj/Qh/gIA=',
-		    'crossorigin' => 'anonymous'
-	    ]
+	    'easy-autocomplete.min.js'
     ];
     public $depends = [
         'yii\web\JqueryAsset'
